0878ea3fc3a75850267fc6e2e1a5fe80288f1cac
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-13 23:03:54 +0800

    feat(components/BaseElements/BaseButton): added

---
+++ components/BaseElements/BaseButton.tsx
@@ -0,0 +1,27 @@
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+import React from "react";
+
+export interface BaseButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  solid?: boolean;
+}
+
+export default function BaseButton(props: BaseButtonProps) {
+  const { className, children, solid } = props;
+  const solidC = "text-white bg-black";
+  const borderC = "border border-black";
+
+  return (
+    <button
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      type="button"
+      className={`rounded px-6 py-1 ${solid ? solidC : borderC} ${
+        className || ""
+      }`}
+    >
+      {children}
+    </button>
+  );
+}
