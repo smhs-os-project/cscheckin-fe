f6267f4254f0aed06b01ff99edb1b4f7f3337536
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-13 23:04:48 +0800

    feat(pages): added a basic homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import LoginComponent from "../components/GoogleLoginComponent/LoginComponent";
 import BasePage from "../components/Page/BasePage";
 
 export default function Home() {
   return (
     <BasePage id="home" title="首頁" full>
-      {/* Your Content Here */}
+      <h1 className="text-3xl font-bold pb-1">登入系統</h1>
+      <h2 className="text-xl pb-4">讓你對各個學生的出缺席情況暸若指掌。</h2>
+      <LoginComponent
+        onLogin={() => undefined}
+        onLogout={() => undefined}
+        onFailure={() => undefined}
+      />
     </BasePage>
   );
 }
