356da167d2d9ce535d3c35232647100b1fbc0317
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-14 12:02:50 +0800

    feat(components/GoogleLoginComponent/LoginComponent): update

--- components/GoogleLoginComponent/LoginComponent.tsx
+++ components/GoogleLoginComponent/LoginComponent.tsx
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
-
 import type {
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
 } from "react-google-login";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
+import { getClientIdList } from "./getClientIdList";
 
 export interface LoginComponentProps {
+  org: string;
   onLogin: (
     response: GoogleLoginResponse | GoogleLoginResponseOffline
   ) => Error | void;
   onLogout: () => Error | void;
   onFailure: (error?: unknown) => void;
+  loginText?: string;
+  logoutText?: string;
 }
 
 export default function LoginComponent({
+  org,
   onLogin,
   onLogout,
   onFailure,
+  loginText = "登入系統",
+  logoutText = "登出系統",
 }: LoginComponentProps) {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const clientIdList = getClientIdList();
+  const clientId = clientIdList[org];
   const [hasLogin, setHasLogin] = useState(false);
   const setOrFailure = (response: Error | void): void => {
     if (response instanceof Error) {
@@ -29,27 +36,28 @@ export default function LoginComponent({
     setHasLogin(true);
   };
 
-  if (!clientId)
-    throw new Error("You should specify NEXT_PUBLIC_GOOGLE_CLIENT_ID");
+  if (clientId) {
+    if (hasLogin)
+      return (
+        <GoogleLogout
+          clientId={clientId}
+          buttonText={logoutText}
+          onLogoutSuccess={() => setOrFailure(onLogout())}
+          onFailure={onFailure}
+          className="w-full sm:w-auto"
+        />
+      );
 
-  if (hasLogin)
     return (
-      <GoogleLogout
+      <GoogleLogin
         clientId={clientId}
-        buttonText="登出系統"
-        onLogoutSuccess={() => setOrFailure(onLogout())}
+        buttonText={loginText}
+        onSuccess={(response) => setOrFailure(onLogin(response))}
         onFailure={onFailure}
         className="w-full sm:w-auto"
       />
     );
+  }
 
-  return (
-    <GoogleLogin
-      clientId={clientId}
-      buttonText="登入系統"
-      onSuccess={(response) => setOrFailure(onLogin(response))}
-      onFailure={onFailure}
-      className="w-full sm:w-auto"
-    />
-  );
+  return <div className="error-message">學校名稱錯誤。</div>;
 }
