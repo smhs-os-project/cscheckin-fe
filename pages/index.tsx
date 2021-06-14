8642be8344ef721455ef16856b988a941c030684
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-14 15:35:38 +0800

    refactor(pages/index): full-width button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import BaseButton from "../components/BaseElements/BaseButton";
 
 import { getClientIdList } from "../components/GoogleLoginComponent/getClientIdList";
 import LoginComponent, {
   Scope,
 } from "../components/GoogleLoginComponent/LoginComponent";
 import HeaderPageCard from "../components/Page/HeaderPageCard";
+import randBackgroundColor from "../utilities/randcolor";
 
 export default function Home() {
   const availableSchool = Object.keys(getClientIdList());
@@ -17,28 +19,33 @@ export default function Home() {
       id="teacher-login-portal"
       title="教師登入系統"
       desc="讓你對各個學生的出缺席情況暸若指掌。"
+      contentPadding={false}
     >
       {school === "" ? (
         <div className="select-school">
-          <h2 className="pb-2 text-2xl text-center md:text-left">
-            選擇登入學校
-          </h2>
-          <div className="grid w-full grid-cols-1 gap-1 justify-items-center md:justify-items-start md:grid-flow-col school-options">
+          <div className="flex flex-col w-full space-yschool-options">
             {availableSchool.map((org) => (
               <div key={`available-school-option-${org}`}>
-                <BaseButton
-                  className="transition-all duration-300 hover:bg-black hover:text-white"
+                <button
+                  className={`w-full px-6 py-10 text-left text-black hover:text-white transition-all duration-300 rounded-none ${randBackgroundColor(
+                    true
+                  )}`}
                   type="button"
                   onClick={() => setSchool(org)}
                 >
-                  {org}
-                </BaseButton>
+                  <div className="flex justify-between">
+                    <div>{org}</div>
+                    <div>
+                      <FontAwesomeIcon icon={faArrowRight} />
+                    </div>
+                  </div>
+                </button>
               </div>
             ))}
           </div>
         </div>
       ) : (
-        <div>
+        <div className="flex content-center justify-center w-full p-10">
           <LoginComponent
             org={school}
             scope={Scope.Teacher}
