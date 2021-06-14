27aafb96d5e74d59516ab888f51b7c4404985734
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 03:22:51 +0800

    feat(pages/admin/checkin/previous): done

---
+++ pages/admin/checkin/previous.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ListChoicePageCard from "../../../components/Page/ListChoicePageCard";
+
+export default function CheckinCreate() {
+  return (
+    <ListChoicePageCard
+      id="checkin-choose-classroom"
+      title="選擇過去紀錄"
+      desc="選擇要查看的過去記錄。"
+    >
+      {[
+        {
+          id: "10345",
+          name: "Hi~",
+          redirect: () => undefined,
+        },
+      ]}
+    </ListChoicePageCard>
+  );
+}
