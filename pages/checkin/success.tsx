483340b151466b7695b274bce4f2c55db756d2bf
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-14 18:23:51 +0800

    refactor(pages/checkin/success): HeaderPageCard

--- pages/checkin/success.tsx
+++ pages/checkin/success.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import BasePage from "../../components/Page/BasePage";
+import HeaderPageCard from "../../components/Page/HeaderPageCard";
 
 export default function CheckinDone() {
   return (
-    <BasePage id="checkin-done" title="完成簽到" full>
-      <h1 className="text-3xl font-bold pb-1">完成簽到</h1>
-      <h2 className="text-xl pb-4">已經簽到。您可留下本憑證以示證明。</h2>
+    <HeaderPageCard
+      id="student-checkin-done"
+      title="完成簽到"
+      desc="已經簽到。您可留下本憑證以示證明。"
+    >
       <p>時間：{new Date().toString()}</p>
       <p>識別 ID：{Math.round(Math.random() * 300000)}</p>
-    </BasePage>
+    </HeaderPageCard>
   );
 }
