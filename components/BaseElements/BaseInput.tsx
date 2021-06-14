594f51348116b4370aa305b3acf1bf4fd44a56d6
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-15 02:50:16 +0800

    feat(componets/BaseElements/BaseInput): type is now configurable

--- components/BaseElements/BaseInput.tsx
+++ components/BaseElements/BaseInput.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export interface BaseInputsProps {
   id: string;
   label: string;
+  type?: string;
   value?: string;
   placeholder?: string;
   onChange?: (value: string) => void;
@@ -11,6 +12,7 @@ export interface BaseInputsProps {
 export default function BaseInput({
   id,
   label,
+  type = "input",
   value,
   placeholder,
   onChange,
@@ -27,7 +29,7 @@ export default function BaseInput({
         </span>
         <input
           id="display-name-input"
-          type="input"
+          type={type}
           className="p-2 border-b border-gray-600 outline-none"
           onChange={(event) => onChange && onChange(event.target.value)}
           value={value}
