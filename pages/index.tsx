99d03fa63199528ca080e4225e7ca2c818629d50
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-14 14:03:32 +0800

    feat(pages/index): animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,11 +19,15 @@ export default function Home() {
 
       {school === "" ? (
         <div className="select-school">
-          <h2 className="text-2xl">選擇學校</h2>
-          <div className="school-options grid grid-flow-col grid-cols-1 md:grid-cols-6 gap-3">
+          <h2 className="text-2xl pb-1">選擇學校</h2>
+          <div className="school-options grid grid-cols-1 md:grid-cols-6 gap-3">
             {availableSchool.map((org) => (
-              <div key={`available-school-option-${org}`} className="w-max">
-                <BaseButton type="button" onClick={() => setSchool(org)}>
+              <div key={`available-school-option-${org}`}>
+                <BaseButton
+                  className="hover:bg-black hover:text-white duration-300 transition-all"
+                  type="button"
+                  onClick={() => setSchool(org)}
+                >
                   {org}
                 </BaseButton>
               </div>
