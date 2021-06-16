3a0929bb7476f4841dbc7548b56cbbebc77473e8
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 09:08:30 +0800

    fix(components/GoogleLoginComponent/idNameMap): api now support it

--- components/GoogleLoginComponent/getClientIdList.tsx
+++ components/GoogleLoginComponent/getClientIdList.tsx
@@ -1,7 +1,6 @@
 import { GetOrganizationList } from "cscheckin-js-sdk";
 import { OrgInfoListResponseSchema } from "cscheckin-js-sdk/dist/types/org_info/resp_org_info";
 import { ValidationError } from "myzod";
-import { map } from "./idNameMap";
 
 export default async function getClientIdList() {
   const srcResponse = await GetOrganizationList();
@@ -12,9 +11,5 @@ export default async function getClientIdList() {
     return [];
   }
 
-  return response.map(({ chinese_name, ...rest }) => ({
-    ...rest,
-    id: map[chinese_name],
-    chinese_name,
-  }));
+  return response;
 }
