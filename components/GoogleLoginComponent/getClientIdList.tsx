ed00b77ad6f4e536e218b4e4cfc424ec1b99394a
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 08:19:44 +0800

    fix(components/GoogleLoginComponent/getClientIdList): don't throw error

--- components/GoogleLoginComponent/getClientIdList.tsx
+++ components/GoogleLoginComponent/getClientIdList.tsx
@@ -1,11 +1,13 @@
 import { GetOrganizationList } from "cscheckin-js-sdk";
 import { OrgInfoListResponseSchema } from "cscheckin-js-sdk/dist/types/org_info/resp_org_info";
+import { ValidationError } from "myzod";
 import { map } from "./idNameMap";
 
 export default async function getClientIdList() {
   const srcResponse = await GetOrganizationList();
-  const response = OrgInfoListResponseSchema.parse(srcResponse);
+  const response = OrgInfoListResponseSchema.try(srcResponse);
 
+  if (response instanceof ValidationError) return [];
   return response.map(({ chinese_name, ...rest }) => ({
     ...rest,
     id: map[chinese_name],
