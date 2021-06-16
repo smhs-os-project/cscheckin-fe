f4d0d5df42145dbb785fcb7936659a869f5acca5
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-16 18:52:13 +0800

    feat(pages/checkin/previous): SDK

--- pages/checkin/previous.tsx
+++ pages/checkin/previous.tsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import type { CourseListResponse } from "cscheckin-js-sdk/dist/types/course/resp_course";
+import { CourseListResponseSchema } from "cscheckin-js-sdk/dist/types/course/resp_course";
+import { GetCoursesList } from "cscheckin-js-sdk";
+import { ValidationError } from "myzod";
 import ListChoicePageCard from "../../components/Page/ListChoicePageCard";
+import { useAuth } from "../../components/AuthStore/utilities";
 
 export default function CheckinPrevious() {
+  const pageId = "checkin-choose-previous";
+  const pageTitle = "選擇過去紀錄";
+  const pageDesc = "選擇要查看的過去記錄。";
+
+  const [auth, loading] = useAuth();
+  const [message, setMessage] = useState<string | null>(null);
+  const [previous, setPrevious] = useState<CourseListResponse>([]);
+
+  useEffect(() => {
+    if (auth) {
+      void GetCoursesList(auth).then((rawCourse) => {
+        const course = CourseListResponseSchema.try(rawCourse);
+
+        if (course instanceof ValidationError) {
+          setMessage("無法取得過去紀錄。");
+        } else {
+          setPrevious(course);
+          setMessage(null);
+        }
+      });
+    } else if (!loading) {
+      setMessage("未登入。");
+    } else {
+      setMessage("正在取得過去紀錄⋯⋯");
+    }
+  }, [auth, loading]);
+
   return (
     <ListChoicePageCard
-      id="checkin-choose-previous"
-      title="選擇過去紀錄"
-      desc="選擇要查看的過去記錄。"
+      id={pageId}
+      title={pageTitle}
+      desc={pageDesc}
+      message={message}
     >
-      {[
-        {
-          id: "10345",
-          name: "Hi~",
-          redirect: () => undefined,
-        },
-      ]}
+      {previous.map(({ name, id }) => ({
+        id: id.toString(),
+        name,
+        redirect: () => console.log(name),
+      }))}
     </ListChoicePageCard>
   );
 }
