d90720517ede18f6f1a46962762a85bd2ed667ad
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-17 10:30:48 +0800

    refactor(pages/checkin/index): done

--- pages/checkin/[org]/[uuid]/index.tsx
+++ pages/checkin/[org]/[uuid]/index.tsx
@@ -1,9 +1,12 @@
-import { GetCourse } from "cscheckin-js-sdk";
+import { GetCourse, Checkin as StudentCheckinMethod } from "cscheckin-js-sdk";
 import type { Organization } from "cscheckin-js-sdk/dist/types/auth/req_auth_token";
+import type { StudentCheckinResponse } from "cscheckin-js-sdk/dist/types/student/resp_checkin";
+import { StudentCheckinResponseSchema } from "cscheckin-js-sdk/dist/types/student/resp_checkin";
+import type { CourseResponse } from "cscheckin-js-sdk/dist/types/course/resp_course";
 import { CourseResponseSchema } from "cscheckin-js-sdk/dist/types/course/resp_course";
 import { ValidationError } from "myzod";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NProgress from "nprogress";
 import LoginComponent, {
   Scope,
@@ -29,49 +32,125 @@ export default function Checkin() {
   const { org, uuid, stage: recvStage } = router.query;
   const [stage, setStage] = useState(Stage.PREPARE);
   const [message, setMessage] = useState<string | null>(null);
-  const [courseName, setCourseName] = useState("");
+  const [course, setCourse] = useState<CourseResponse | null>(null);
+  const [checkinInfo, setCheckinInfo] = useState<StudentCheckinResponse | null>(
+    null
+  );
 
-  switch (stage) {
-    case Stage.PREPARE: {
-      if (!(typeof org === "string" && typeof uuid === "string")) {
-        return null; // router needs a while to get the query data
-      }
-
-      void GetCourse(uuid)
-        .then((course) => {
-          const cs = CourseResponseSchema.try(course);
-
-          if (cs instanceof ValidationError) {
-            console.error(course);
-            return Promise.reject(new Error("課程不存在或尚未開放。"));
-          }
-
-          if (typeof recvStage === "string" && /^\d+$/.exec(recvStage)) {
-            setStage(Number(recvStage) as Stage);
-          } else {
-            setStage(Stage.LOGIN);
-            setCourseName(cs.name);
-            setMessage(null);
-          }
-
-          return undefined;
-        })
-        .catch((error: Error) => {
-          setStage(Stage.FAILED);
-          setMessage(error.message);
-        });
-      break;
+  /* NProgress */
+  useEffect(() => {
+    switch (stage) {
+      case Stage.PREPARE:
+      case Stage.CHECK_IF_REGISTER:
+      case Stage.CHECK_IN:
+        NProgress.start();
+        break;
+      case Stage.FAILED:
+      case Stage.LOGIN:
+      case Stage.REQUIRE_TO_REGISTER:
+      case Stage.SUCCESS:
+      default:
+        NProgress.done();
+        break;
     }
+  }, [stage]);
+
+  // logic
+  useEffect(() => {
+    switch (stage) {
+      case Stage.PREPARE: {
+        if (!(typeof org === "string" && typeof uuid === "string")) {
+          return undefined; // router needs a while to get the query data
+        }
+
+        void GetCourse(uuid)
+          .then((rCourse) => {
+            const cs = CourseResponseSchema.try(rCourse);
+
+            if (cs instanceof ValidationError) {
+              console.error(rCourse);
+              return Promise.reject(new Error("課程不存在或尚未開放。"));
+            }
+
+            if (typeof recvStage === "string" && /^\d+$/.exec(recvStage)) {
+              setStage(Number(recvStage) as Stage);
+            } else {
+              setCourse(cs);
+              setStage(Stage.LOGIN);
+              setMessage(null);
+            }
+
+            return undefined;
+          })
+          .catch((error: Error) => {
+            setMessage(error.message);
+            setStage(Stage.FAILED);
+          });
+
+        break;
+      }
+      case Stage.CHECK_IF_REGISTER: {
+        void AuthStore.retrieve()
+          .then((auth) => {
+            if (!auth)
+              return Promise.reject(new Error("無法取得身份識別資訊。"));
+            return auth.userInfo();
+          })
+          .then((userInfo) => {
+            if (userInfo?.student) setStage(Stage.CHECK_IN);
+            else setStage(Stage.REQUIRE_TO_REGISTER);
+          })
+          .catch((error: Error) => {
+            setMessage(error.message);
+            setStage(Stage.FAILED);
+          });
+        break;
+      }
+      case Stage.REQUIRE_TO_REGISTER:
+        void router.push(`/register?redirect='/checkin/${org}/${uuid}?stage=4`);
+        break;
+      case Stage.CHECK_IN:
+        void AuthStore.retrieve()
+          .then((auth) => {
+            if (!auth)
+              return Promise.reject(new Error("無法取得身份識別資訊。"));
+            if (!course) return Promise.reject(new Error("無法取得課程資訊。"));
+
+            return StudentCheckinMethod(course.id, auth);
+          })
+          .then((rCheckin) => {
+            const checkin = StudentCheckinResponseSchema.try(rCheckin);
+
+            if (checkin instanceof ValidationError) {
+              return Promise.reject(new Error("簽到失敗。"));
+            }
+
+            setCheckinInfo(checkin);
+            setStage(Stage.SUCCESS);
+            return Promise.resolve();
+          })
+          .catch((error: Error) => {
+            setMessage(error.message);
+            setStage(Stage.FAILED);
+          });
+        break;
+      default:
+        break;
+    }
+
+    return undefined;
+  }, [stage, org, uuid]);
+
+  // views
+  switch (stage) {
     case Stage.LOGIN:
-      return (
-        <HeaderPageCard
-          id={pageId}
-          title={pageTitle}
-          desc={`簽到${courseName}。`}
-        >
-          {message ? (
-            <p>{message}</p>
-          ) : (
+      if (course) {
+        return (
+          <HeaderPageCard
+            id={pageId}
+            title={pageTitle}
+            desc={`簽到${course?.name}。`}
+          >
             <LoginComponent
               // we assumed user sent the valid organization,
               // since we will check if it is valid within LoginComponent.
@@ -82,44 +161,29 @@ export default function Checkin() {
               }}
               loginText="點此簽到"
             />
-          )}
-        </HeaderPageCard>
-      );
-    case Stage.CHECK_IF_REGISTER:
-      NProgress.start();
-      void AuthStore.retrieve()
-        .then((auth) => {
-          if (!auth) return Promise.reject(new Error("未取得登入資料。")); // TODO
-          return auth.userInfo();
-        })
-        .then((userInfo) => {
-          if (userInfo?.student) setStage(Stage.CHECK_IN);
-          else setStage(Stage.REQUIRE_TO_REGISTER);
-        })
-        .catch((error: Error) => {
-          setMessage(error.message);
-          setStage(Stage.FAILED);
-        });
-      break;
-    case Stage.REQUIRE_TO_REGISTER:
-      void router.push(`/register?redirect='/checkin/${org}/${uuid}?stage=4`);
-      break;
-    case Stage.CHECK_IN:
-      setStage(Stage.SUCCESS); // TODO
+          </HeaderPageCard>
+        );
+      }
+      setMessage("無法擷取課程資訊。");
+      setStage(Stage.FAILED);
       break;
     case Stage.SUCCESS:
-      NProgress.done();
-      return (
-        <HeaderPageCard
-          id={pageId}
-          title={pageTitle}
-          desc="簽到完成。您可留下本憑證以示證明。"
-        >
-          <p>現在時間：{new Date().toString()}</p>
-          <p>識別 ID：{Math.round(Math.random() * 300000)}</p>
-        </HeaderPageCard>
-      );
-    case Stage.FAILED:
+      if (checkinInfo) {
+        return (
+          <HeaderPageCard
+            id={pageId}
+            title={pageTitle}
+            desc="簽到完成。您可留下本憑證以示證明。"
+          >
+            <p>簽到時間：{checkinInfo.updated_at}</p>
+            <p>識別 ID：{checkinInfo?.course_id}</p>
+          </HeaderPageCard>
+        );
+      }
+
+      setMessage("無法取得簽到後續。");
+      setStage(Stage.FAILED);
+      break;
     default:
       break;
   }
