9b9dc4e03f257ea533d270fb036330aa99eae045
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-18 12:59:51 +0800

    feat(components/Navbar/Navbar): Home & Previous button
    fixed #18

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Navbar() {
+  const router = useRouter();
+
   return (
-    <div className="p-8 grid grid-col-1 md:grid-col-3 content-center items-center">
-      <div className="col-start-1 col-end-1">
-        <Image src="/icon.svg" width="36em" height="36em" />
+    <div className="grid items-center content-center p-8 grid-col-1 md:grid-col-3">
+      <div className="flex items-center col-start-1 col-end-1">
+        {router.pathname !== "/" && (
+          <button
+            type="button"
+            className="h-full mr-5 focus:outline-none"
+            onClick={() => router.back()}
+          >
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+        )}
+        <button type="button" className="focus:outline-none">
+          <Link href="/">
+            <Image src="/icon.svg" width="36em" height="36em" />
+          </Link>
+        </button>
       </div>
       <div className="col-start-3 col-end-3 justify-self-end">
         {/* Your Content */}
