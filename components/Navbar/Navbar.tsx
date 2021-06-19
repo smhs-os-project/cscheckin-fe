d47dabc724d6fdfe34f959509b8a9f5fe2e59137
yc97463 <youualan87@gmail.com>
2021-06-19 17:04:35 +0800

    feat(component/Navbar/Navbar): application title
    Co-authored-by: pan93412 <pan93412@gmail.com>
    Sz-Wei Chen<swchen1217@gmail.com>

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,7 +10,7 @@ export default function Navbar() {
 
   return (
     <div className="grid items-center content-center p-8 grid-col-1 md:grid-col-3">
-      <div className="flex items-center col-start-1 col-end-1">
+      <div className="flex content-center items-center col-start-1 col-end-1">
         {router.pathname !== "/" && (
           <button
             type="button"
@@ -19,14 +20,16 @@ export default function Navbar() {
             <FontAwesomeIcon icon={faArrowLeft} />
           </button>
         )}
-        <button
-          type="button"
-          className="focus:outline-none"
-          onClick={() => router.push("/")}
-        >
-          <Image src="/icon.svg" width="36em" height="36em" />
+        <button type="button" className="focus:outline-none">
+          <Link href="/">
+            <div className="flex content-center items-center">
+              <div className="w-max h-max">
+                <Image src="/icon.svg" width="36em" height="36em" />
+              </div>
+              <div className="w-min h-min text-xl font-bold ml-2 pb-1">CSCheckin</div>
+            </div>
+          </Link>
         </button>
-        <p className="pb-1 text-2xl font-bold ml-2">CSCheckin</p>
       </div>
       <div className="col-start-3 col-end-3 justify-self-end">
         {/* Your Content */}
