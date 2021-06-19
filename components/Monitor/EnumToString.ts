e42f029488276e2dfd942db1e8a855254b6b479d
yc97463 <youualan87@gmail.com>
2021-06-19 16:57:24 +0800

    fix(components/Monitor/EnumToString): friendly sentence break

--- components/Monitor/EnumToString.ts
+++ components/Monitor/EnumToString.ts
@@ -5,9 +5,9 @@ export function getCourseStatus(state: CheckinState) {
     case CheckinState.ON_TIME:
       return "目前簽到的學生均標記為「✅ 準時」。";
     case CheckinState.LATE:
-      return "目前已超過「簽到準時時間」，<br />後續簽到的學生將標記為「⚠️ 遲到」。";
+      return "目前已超過「簽到準時時間」， 後續簽到的學生將標記為「⚠️ 遲到」。";
     default:
-      return "目前簽到已結束，<br />未到的學生將標記為「❌ 未到」，且不能再補簽。";
+      return "目前簽到已結束， 未到的學生將標記為「❌ 未到」，且不能再補簽。";
   }
 }
 
@@ -16,7 +16,7 @@ export function getCheckinStatusIcon(state: CheckinState) {
     case CheckinState.ON_TIME:
       return "✅";
     case CheckinState.LATE:
-      return "⚠";
+      return "⚠️";
     default:
       return "❌";
   }
