2613da73b191d1217921fc58eb340a5e1d488e29
yc97463 <youualan87@gmail.com>
2021-06-21 09:47:32 +0800

    feat(components/Support/FlowButton): add sponsor icon

--- components/Support/FlowButton.tsx
+++ components/Support/FlowButton.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { faBug, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import { faBug, faHandHoldingUsd, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { AuthUserResponse } from "cscheckin-js-sdk/dist/types";
 import { randTextColor } from "../../utilities/randcolor";
 import { useAuth } from "../AuthStore/utilities";
+import Link from "next/link";
 
 export default function SupportBtn() {
   const router = useRouter();
@@ -17,6 +18,18 @@ export default function SupportBtn() {
 
   return (
     <div style={{ marginRight: ".7rem" }}>
+      <button type="button" className="rounded-full">
+        <a href="https://cscin.tk/sponsor" target="_blank" rel="noreferrer">
+          <div className="flex items-center" style={{ marginRight: ".7rem" }}>
+            <FontAwesomeIcon
+              icon={faHandHoldingUsd}
+              size="2x"
+              className={`transition-all duration-300 ${randTextColor()}`}
+            />
+          </div>
+        </a>
+      </button>
+
       <button type="button" className="rounded-full">
         <a href="https://cscin.tk/join" target="_blank" rel="noreferrer">
           <div className="flex items-center" style={{ marginRight: ".7rem" }}>
