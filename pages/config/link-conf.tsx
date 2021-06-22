83684d50926427b7343d84498cadec9d857d93c5
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-22 22:15:35 +0800

    feat(pages/config/link-conf): cleanup

--- pages/config/link-conf.tsx
+++ pages/config/link-conf.tsx
@@ -1,11 +1,15 @@
 import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRouter } from "next/router";
 import type { ReactNode } from "react";
 import React, { useState } from "react";
 import BaseButton from "../../components/BaseElements/BaseButton";
 import BaseInput from "../../components/BaseElements/BaseInput";
+import { END_DURATION, LATE_DURATION } from "../../components/LocalDB/consts";
+import { useConfig } from "../../components/LocalDB/utilities";
 import HeaderPageCard from "../../components/Page/HeaderPageCard";
+
+const DEFAULT_LATE_DURATION = "10:00";
+const DEFAULT_END_DURATION = "60:00";
 
 interface CheckinComponentProps {
   id: string;
@@ -59,67 +63,60 @@ export default function CheckinCreate() {
   const checkinOntimeDurationId = "checkin-ontime-duration";
   const checkinEndDurationId = "checkin-end-duration";
 
-  const router = useRouter();
-  const { cid } = router.query;
-
   const [warnMessage, warn] = useWarn();
-  const [ontimeDuration, setOntimeDuration] = useState("10");
-  const [endDuration, setEndDuration] = useState("55");
+  const [iLateDuration, setILateDuration] = useState(DEFAULT_LATE_DURATION);
+  const [iEndDuration, setIEndDuration] = useState(DEFAULT_END_DURATION);
+  const [lateDuration, setLateDuration] = useConfig(LATE_DURATION);
+  const [endDuration, setEndDuration] = useConfig(END_DURATION);
 
   return (
     <HeaderPageCard
-      id="checkin-choose-classroom"
+      id="checkin-duration-configure"
       title="設定簽到連結"
-      desc="選擇遲到與結束時間。設定後不能修改。"
+      desc="選擇未來簽到連結使用的遲到與結束時間。可隨時修改。"
     >
-      {typeof cid === "string" ? (
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <CheckinComponent
+          id={checkinOntimeDurationId}
+          help={() => {
+            warn("超過「準時簽到時限」後簽到的學生都會被標記為「⚠️ 遲到」。");
           }}
+          suffix={<span className="mr-1">分分：秒秒</span>}
         >
-          <CheckinComponent
-            id={checkinOntimeDurationId}
-            help={() => {
-              warn("超過「準時簽到時限」後簽到的學生都會被標記為「⚠️ 遲到」。");
-            }}
-            suffix={<span className="mr-1">分鐘內</span>}
-          >
-            <BaseInput
-              id={`${checkinEndDurationId}-input`}
-              label="準時簽到時限"
-              type="number"
-              value={ontimeDuration}
-              onChange={setOntimeDuration}
-            />
-          </CheckinComponent>
-          <CheckinComponent
-            id={checkinEndDurationId}
-            help={() => {
-              warn(
-                "超過「課程簽到時限」後，學生就不能補簽。系統會將未簽到學生標記成「❌ 未到」。"
-              );
-            }}
-            suffix={<span className="mr-1">分鐘後</span>}
-          >
-            <BaseInput
-              id={`${checkinEndDurationId}-input`}
-              label="課程簽到時限"
-              type="number"
-              value={endDuration}
-              onChange={setEndDuration}
-            />
-          </CheckinComponent>
-          <div className="my-3 text-red-600 warning-message">{warnMessage}</div>
-          <div className="flex justify-between">
-            <BaseButton solid submit>
-              儲存
-            </BaseButton>
-          </div>
-        </form>
-      ) : (
-        <p>未指定 Classroom ID。請回到管理選單重新設定。</p>
-      )}
+          <BaseInput
+            id={`${checkinEndDurationId}-input`}
+            label="準時簽到時限"
+            value={lateDuration ?? DEFAULT_LATE_DURATION}
+            onChange={setLateDuration}
+          />
+        </CheckinComponent>
+        <CheckinComponent
+          id={checkinEndDurationId}
+          help={() => {
+            warn(
+              "超過「課程簽到時限」後，學生就不能補簽。系統會將未簽到學生標記成「❌ 未到」。"
+            );
+          }}
+          suffix={<span className="mr-1">分分：秒秒</span>}
+        >
+          <BaseInput
+            id={`${checkinEndDurationId}-input`}
+            label="課程簽到時限"
+            value={endDuration ?? DEFAULT_END_DURATION}
+            onChange={setEndDuration}
+          />
+        </CheckinComponent>
+        <div className="my-3 text-red-600 warning-message">{warnMessage}</div>
+        <div className="flex justify-between">
+          <BaseButton solid submit>
+            儲存
+          </BaseButton>
+        </div>
+      </form>
     </HeaderPageCard>
   );
 }
