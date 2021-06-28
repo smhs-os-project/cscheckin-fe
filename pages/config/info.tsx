02e455fb16554c5d8afe495562d35f2aef77c5a2
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-29 01:59:20 +0800

    feat(pages/config/info): "Cancel"

--- pages/config/info.tsx
+++ pages/config/info.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
 import HeaderPageCard from "../../components/Page/HeaderPageCard";
 import BaseInput from "../../components/BaseElements/BaseInput";
 import BaseButton from "../../components/BaseElements/BaseButton";
 
 export default function CSCConfigInfoSetup() {
+  const router = useRouter();
+
   return (
     <HeaderPageCard
       id="config-info-setup"
@@ -17,6 +20,9 @@ export default function CSCConfigInfoSetup() {
         <BaseInput id="number" label="座號" required />
         <BaseButton solid submit className="mt-4">
           儲存
+        </BaseButton>
+        <BaseButton className="mt-4 ml-2" onClick={() => router.back()}>
+          取消
         </BaseButton>
       </form>
     </HeaderPageCard>
