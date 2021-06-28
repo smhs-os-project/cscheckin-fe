4cecba11e5e2689c4b9b46c654c3600c6ca3bc8d
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-28 20:23:45 +0800

    fix(pages/dev/config/index): make the button available to use

--- pages/dev/config/index.tsx
+++ pages/dev/config/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
 import ListChoicePageCard from "../../../components/Page/ListChoicePageCard";
 
 export default function CSCConfiguration() {
+  const router = useRouter();
+
   return (
     <ListChoicePageCard
       id="csc-configuration"
@@ -14,12 +17,16 @@ export default function CSCConfiguration() {
         {
           id: "info-setup",
           name: "設定班級座號",
-          redirect: () => {},
+          redirect: async () => {
+            await router.push("/config/info");
+          },
         },
         {
           id: "duration-setup",
           name: "設定遲到與結束簽到時間",
-          redirect: () => {},
+          redirect: async () => {
+            await router.push("/config/checkin-duration");
+          },
         },
       ]}
     </ListChoicePageCard>
