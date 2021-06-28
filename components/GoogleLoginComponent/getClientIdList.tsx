7b7b3a4cad53e55d953d6eb197f753239dc14a99
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-28 18:08:36 +0800

    refactor(components/GoogleLoginComponent/getClientIdList): deprecated
    it will ONLY return an empty array now.

--- components/GoogleLoginComponent/getClientIdList.tsx
+++ components/GoogleLoginComponent/getClientIdList.tsx
@@ -1,18 +1,4 @@
-import { GetOrganizationList } from "cscheckin-js-sdk";
-import { OrgInfoListResponseSchema } from "cscheckin-js-sdk/dist/types";
-import { ValidationError } from "myzod";
-import Sentry from "../../utilities/sentry";
-
+/** @deprecated SHOULD NOT BE USED IN THE NEW CODE */
 export default async function getClientIdList() {
-  const srcResponse = await GetOrganizationList();
-  const response = OrgInfoListResponseSchema.try(srcResponse);
-
-  if (response instanceof ValidationError) {
-    Sentry.captureMessage(
-      "getClientIdList: failed to retrieve the organization list"
-    );
-    return [];
-  }
-
-  return response;
+  return [];
 }
