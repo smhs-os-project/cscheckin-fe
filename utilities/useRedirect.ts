4dacb9f951eb45d6234dc00ae00e44cbe6f9c0f2
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-28 20:21:14 +0800

    feat(utilities/useRedirect): done!

---
+++ utilities/useRedirect.ts
@@ -0,0 +1,24 @@
+import type { NextRouter } from "next/router";
+import { useRouter } from "next/router";
+
+export default function useRedirect(
+  defaultRedirect?: string,
+  router?: NextRouter
+): { redirect: () => Promise<{ success: boolean }> } {
+  const iRouter = router || useRouter();
+  const { redirect } = iRouter.query;
+
+  return {
+    async redirect() {
+      if (typeof redirect === "string") {
+        await iRouter.push(redirect);
+      } else if (defaultRedirect) {
+        await iRouter.push(defaultRedirect);
+      } else {
+        return { success: false };
+      }
+
+      return { success: true };
+    },
+  };
+}
