86af3b196a02b474a81e744f874df3bd78c48c14
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-29 00:26:34 +0800

    feat(pages/index): done!

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import useAuth from "../components/AuthStore/useAuth";
+
+export default function CSCIndex() {
+  const router = useRouter();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    if (auth) void router.push("/checkin/manage");
+  }, [auth]);
+
+  return null;
+}
