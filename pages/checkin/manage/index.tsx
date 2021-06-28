6052088e1a67bbf6d161bac33d318641d29988ac
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-29 00:26:24 +0800

    feat(pages/checkin/manage): add router.push to redirects

--- pages/checkin/manage/index.tsx
+++ pages/checkin/manage/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
 import ListChoicePageCard from "../../../components/Page/ListChoicePageCard";
 
 export default function CSCManageIndex() {
+  const router = useRouter();
+
   return (
     <ListChoicePageCard
       id="csc-manage-home"
@@ -14,12 +17,12 @@ export default function CSCManageIndex() {
         {
           id: "create-new",
           name: "建立新的簽到連結",
-          redirect: () => {},
+          redirect: async () => router.push("/checkin/manage/create"),
         },
         {
           id: "see-previous",
           name: "查看以往簽到連結",
-          redirect: () => {},
+          redirect: async () => router.push("/checkin/manage/previous"),
         },
       ]}
     </ListChoicePageCard>
