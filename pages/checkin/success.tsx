8313bc141ed3cde3ccb58a9116cbba02eb66edb7
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-30 00:30:11 +0800

    feat(pages/checkin/success): complete

--- pages/checkin/success.tsx
+++ pages/checkin/success.tsx
@@ -1,11 +1,26 @@
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import React, { useEffect, useState } from "react";
-import type { AuthUserResponse } from "cscheckin-js-sdk/dist/types";
+import type {
+  AuthUserResponse,
+  CheckinResponse,
+} from "cscheckin-js-sdk/dist/types";
+import { CheckinResponseSchema } from "cscheckin-js-sdk/dist/types";
+import { ValidationError } from "myzod";
 import HeaderPageCard from "../../components/Page/HeaderPageCard";
 import useAuth from "../../components/AuthStore/useAuth";
 import { Scope } from "../../components/GoogleLoginComponent/LoginComponent";
+import SessionDB from "../../components/SessionDB";
+import { checkinData } from "../../components/SessionDB/consts";
+import ErrorPage from "../../components/Page/ErrorPage";
+import getStateBrief from "../../utilities/getStateBrief";
 
-export default function CSCStudentCheckinSuccess() {
+const sdb = SessionDB.getInstance();
+
+export function InnerCSCStudentCheckinSuccess({
+  data: cdata,
+}: {
+  data: CheckinResponse;
+}) {
   const { auth } = useAuth(true, Scope.Student);
   const [userInfo, setUserInfo] = useState<AuthUserResponse | null>(null);
 
@@ -25,6 +40,8 @@ export default function CSCStudentCheckinSuccess() {
     >
       {userInfo ? (
         <>
+          <p>課程 ID：{cdata.course_id}</p>
+          <p>簽到狀態：{getStateBrief(cdata.state)}</p>
           <p>姓名：{userInfo.name}</p>
           <p>信箱：{userInfo.email}</p>
         </>
@@ -34,5 +51,34 @@ export default function CSCStudentCheckinSuccess() {
 
       <p>時間：{new Date().toLocaleString()}</p>
     </HeaderPageCard>
+  );
+}
+
+export default function CSCStudentCheckinSuccess() {
+  const [data, setData] = useState<CheckinResponse | null>(null);
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setData(
+      sdb.getObj(
+        checkinData,
+        (val: unknown): val is CheckinResponse =>
+          !(CheckinResponseSchema.try(val) instanceof ValidationError)
+      )
+    );
+    setReady(true);
+  }, [setData]);
+
+  if (!ready) return null;
+
+  if (data) {
+    sdb.remove(checkinData);
+    return <InnerCSCStudentCheckinSuccess data={data} />;
+  }
+  return (
+    <ErrorPage
+      errorMessage="在簽到之前，您不能進入簽到完成介面。"
+      errorDetails={`session 沒有 ${checkinData} 這個 key`}
+    />
   );
 }
