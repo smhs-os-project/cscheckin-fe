800801a51fc66d233d988aaf79ac97b9e402374a
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-29 18:15:21 +0800

    feat(pages/sso/student): require setting up class & number

--- pages/sso/student.tsx
+++ pages/sso/student.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
 import HeaderPageCard from "../../components/Page/HeaderPageCard";
 import LoginComponent, {
   Scope,
@@ -7,11 +8,29 @@ import LoginComponent, {
 import useError from "../../utilities/useError";
 import ErrorPage from "../../components/Page/ErrorPage";
 import useRedirect from "../../utilities/useRedirect";
+import useUserInfo from "../../utilities/useUserInfo";
 
 export default function CSCSSOStudentLogin() {
+  const router = useRouter();
   const [loggedInFlag, setLoggingInFlag] = useState(false);
   const [error, setError] = useError();
-  const { redirect } = useRedirect("/");
+  const { redirectTo } = useRedirect("/");
+  const { userInfo, ready: userInfoReady } = useUserInfo();
+
+  useEffect(() => {
+    if (!userInfoReady || !loggedInFlag) return undefined;
+
+    if (userInfo) {
+      if (redirectTo) void router.push(redirectTo);
+    } else {
+      const redirectParameter = redirectTo
+        ? `?redirect=${encodeURIComponent(redirectTo)}`
+        : "";
+      void router.push(`/config/info${redirectParameter}`);
+    }
+
+    return undefined;
+  }, [userInfoReady, loggedInFlag, redirectTo, router, userInfo]);
 
   if (error) {
     return (
@@ -33,7 +52,6 @@ export default function CSCSSOStudentLogin() {
           scope={Scope.Student}
           onLogin={async () => {
             setLoggingInFlag(true);
-            await redirect();
           }}
           onFailure={async (e) => setError(e)}
         />
