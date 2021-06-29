2eefd95a1b6dcfc232b850fff7e2f0f6043effa5
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-30 03:15:06 +0800

    feat(components/LocalDB/utils): ready

--- components/LocalDB/utilities.ts
+++ components/LocalDB/utilities.ts
@@ -7,12 +7,14 @@ const localDB = LocalDB.getInstance();
  * The config manager of a key.
  *
  * @param key The configuration key.
- * @returns [the value, the function to set the value]
+ * @returns [the value, the function to set the value, ready?]
  */
 export function useConfig(
   key: string
-): [string | null, (value: string) => void] {
+): [string | null, (value: string) => void, boolean] {
   const [value, setValue] = useState<string | null>(null);
+  const [ready, setReady] = useState(false);
+
   const setter = (newValue: string) => {
     setValue(localDB.set(key, newValue).get(key));
   };
@@ -23,9 +25,11 @@ export function useConfig(
     window.addEventListener("storage", (ev) => {
       if (ev.key === key) setValue(ev.newValue);
     });
+
+    setReady(true);
   }, [setValue, key]);
 
-  return [value, setter];
+  return [value, setter, ready];
 }
 
 export default {};
