d66922818b42a2f555151cb4e4d4cb806566cf13
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-29 17:19:04 +0800

    feat(pages/config/info): done!

--- pages/config/info.tsx
+++ pages/config/info.tsx
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 import HeaderPageCard from "../../components/Page/HeaderPageCard";
 import BaseInput from "../../components/BaseElements/BaseInput";
 import BaseButton from "../../components/BaseElements/BaseButton";
+import useAuth from "../../components/AuthStore/useAuth";
+import { Scope } from "../../components/GoogleLoginComponent/LoginComponent";
+import useError from "../../utilities/useError";
+import useRedirect from "../../utilities/useRedirect";
+import ErrorPage from "../../components/Page/ErrorPage";
 
 export default function CSCConfigInfoSetup() {
   const router = useRouter();
+  const { auth, error: authError } = useAuth(true, Scope.Student);
+  const { redirect } = useRedirect("/config");
+  const [error, setError] = useError();
+  const [userClass, setUserClass] = useState("");
+  const [userNumber, setUserNumber] = useState("");
+  const [hasInitiated, setHasInitiated] = useState(false);
+
+  useEffect(() => {
+    auth
+      ?.userInfo()
+      .then((userInfo) => {
+        if (
+          userInfo?.student &&
+          !hasInitiated &&
+          userClass === "" &&
+          userNumber === ""
+        ) {
+          const { class: userOriginalClass, number: userOriginalNumber } =
+            userInfo.student;
+
+          setUserClass(userOriginalClass);
+          setUserNumber(userOriginalNumber);
+          setHasInitiated(true);
+        }
+      })
+      .catch(() => {
+        /* ignore it */
+      });
+  }, [auth, userClass, userNumber, hasInitiated]);
+
+  useEffect(() => {
+    if (authError) setError(authError);
+  }, [authError, setError]);
+
+  if (error) {
+    return (
+      <ErrorPage errorMessage={error.message} errorDetails={error.details} />
+    );
+  }
 
   return (
     <HeaderPageCard
@@ -15,9 +59,46 @@ export default function CSCConfigInfoSetup() {
       desc="重新設定您的班級和座號。"
       icon={faCog}
     >
-      <form>
-        <BaseInput id="class" label="班級" required />
-        <BaseInput id="number" label="座號" required />
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          if (auth) {
+            const success = await auth.setIdentity(userClass, userNumber);
+
+            if (!success) {
+              setError({
+                message:
+                  "輸入內容有誤或是伺服器錯誤，以致無法更改班級或座號 ; (",
+                details: `Class: ${encodeURIComponent(
+                  userClass
+                )}; Number: ${encodeURIComponent(userNumber)}`,
+              });
+            } else {
+              await redirect();
+            }
+          } else {
+            setError({
+              message: "身份驗證失敗。試試看重新載入或重開瀏覽器？",
+              details: `${auth}`,
+            });
+          }
+        }}
+      >
+        <BaseInput
+          id="class"
+          label="班級"
+          value={userClass}
+          onChange={setUserClass}
+          required
+        />
+        <BaseInput
+          id="number"
+          label="座號"
+          value={userNumber}
+          onChange={setUserNumber}
+          required
+        />
         <BaseButton solid submit className="mt-4">
           儲存
         </BaseButton>
