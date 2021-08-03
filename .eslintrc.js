e5ec08379f562b2dcdf7333fccc695e91411a730
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-04 01:42:21 +0800

    feat(lint): enable react-hook check

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     'airbnb-typescript',
