96103f0a600e982743aaebd37cc8ed57cd7b78a5
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-04 02:08:05 +0800

    feat(config/tailwind): import our design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,32 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      color: {
+        primary: "#263238",
+        secondary: "#EEEEEE",
+        accent: "#0A85F6",
+        positive: "#12B981",
+        neutral: "#F49E0B",
+        negative: "#EE4444",
+      },
+      fontSize: {
+        h1: "36px",
+        h2: "24px",
+        body: "16px",
+        button: "16px",
+      },
+      letterSpacing: {
+        h1: "0.25px",
+        body: "0.5px",
+        button: "0.5px",
+      },
+      fontWeight: {
+        header: 700,
+        body: 400,
+        button: 700,
+      },
+    },
   },
   plugins: [],
 };
