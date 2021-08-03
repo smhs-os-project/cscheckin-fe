633f1f7e511e836429c14800b55c27d96f157e35
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-04 03:10:03 +0800

    feat(components/Navbar): our new icon

--- components/Navbar/NavbarContent.tsx
+++ components/Navbar/NavbarContent.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import BaseButton from "../Elements/Button/BaseButton";
 
 // export interface NavbarContentProps {}
 
 export default function NavbarContent() {
-  return <>{/* Your Content */}</>;
+  return (
+    <div className="flex items-center">
+      <BaseButton solid>登入來建立簽到連結</BaseButton>
+    </div>
+  );
 }
--- components/Navbar/NavbarIcon.tsx
+++ components/Navbar/NavbarIcon.tsx
@@ -3,5 +3,10 @@ import Image from "next/image";
 import icon from "../../public/icon.svg";
 
 export default function NavbarIcon() {
-  return <Image src={icon as StaticImageData} width="36em" height="36em" />;
+  return (
+    <div className="flex font-header items-center space-x-3 text-h2">
+      <Image src={icon as StaticImageData} width="36em" height="36em" />
+      <div>CSCheckin</div>
+    </div>
+  );
 }
