f4ad74ec52a3fe7c00aed960845f45d6a2ee7f82
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 02:03:33 +0800

    feat(components/Navbar/Navbar): useNavbarVariant()

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,30 @@
-import React from "react";
-import NavbarContent from "./NavbarContent";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import NavbarContent, { NavbarContentVariant } from "./NavbarContent";
 import NavbarIcon from "./NavbarIcon";
 
+function useNavbarVariant() {
+  const router = useRouter();
+  const [variant, setVariant] = useState(NavbarContentVariant.NOT_LOGGED_IN);
+
+  useEffect(() => {
+    if (router && router.asPath === "/")
+      setVariant(NavbarContentVariant.HOMEPAGE);
+  }, [router]);
+
+  return variant;
+}
+
 export default function Navbar() {
+  const variant = useNavbarVariant();
+
   return (
     <div className="p-8 grid grid-col-1 md:grid-col-3 content-center items-center">
       <div className="col-start-1 col-end-1">
         <NavbarIcon />
       </div>
       <div className="col-start-3 col-end-3 justify-self-end">
-        <NavbarContent />
+        <NavbarContent variant={variant} />
       </div>
     </div>
   );
