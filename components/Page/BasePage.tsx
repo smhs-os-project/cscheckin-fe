a81ed9506460c5495d4412d85b57505dd5da1a72
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 01:42:52 +0800

    refactor(components/Page/BasePage): get fonts from remote

--- components/Page/BasePage.tsx
+++ components/Page/BasePage.tsx
@@ -26,6 +26,20 @@ export default function BasePage({
         <title>
           {PRODUCT_NAME} - {title}
         </title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       {navbar && (
         <section>
