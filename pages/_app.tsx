f1a590e84c0182a94899ae31bfb7d4c874b0a5b7
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 01:43:15 +0800

    refactor(pages/_app): remove local fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from "react";
 import Router, { useRouter } from "next/router";
 import NProgress from "nprogress";
 import type { AppProps } from "next/app";
-import "@fontsource/noto-sans-tc";
-import "@fontsource/pt-sans";
 import "../styles/global.css";
 import "../styles/nprogress.css";
 import { ENABLE_GA } from "../consts";
