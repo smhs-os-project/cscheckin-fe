0571a5b0cf2303a292261c9dead6a1a6b91d88ba
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 01:44:56 +0800

    feat(pages/_document): move Google Fonts from BasePage.tsx to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
 import { GA_TRACKING_ID } from "../utilities/Analytics/analytics";
 import { ENABLE_GA } from "../consts";
 
@@ -12,6 +11,20 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
           {ENABLE_GA && (
             <>
               {/* Global Site Tag (gtag.js) - Google Analytics */}
