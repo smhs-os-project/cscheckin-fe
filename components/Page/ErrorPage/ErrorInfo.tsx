64587048e482e8670512698b8f142944301d8a13
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 02:56:12 +0800

    feat(components/Page/ErrorPage): done!

--- components/Page/ErrorPage/ErrorInfo.tsx
+++ components/Page/ErrorPage/ErrorInfo.tsx
@@ -13,13 +13,17 @@ export default function ErrorInfo({
     <table>
       <tbody>
         <tr>
-          <th className="text-left pr-3">發生時間</th>
+          <th className="text-left pr-3 text-auxiliary opacity-50">發生時間</th>
           <td className="max-h-48">{occurredDate.toLocaleString()}</td>
         </tr>
         {occurredDetails && (
           <tr>
-            <th className="text-left pr-3">詳細資訊</th>
-            <td className="max-h-48">{occurredDetails}</td>
+            <th className="text-left pr-3 text-auxiliary opacity-50">
+              詳細資訊
+            </th>
+            <td className="max-h-48">
+              <pre className="details-block">{occurredDetails}</pre>
+            </td>
           </tr>
         )}
       </tbody>
--- components/Page/ErrorPage/index.tsx
+++ components/Page/ErrorPage/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import dynamic from "next/dynamic";
 import HeaderPageCard from "../HeaderPageCard";
-import BaseButtonGroup from "../../Elements/Button/Group/BaseButtonGroup";
+import LargeButton from "../../Elements/Button/LargeButton";
 
 const ErrorInfo = dynamic(() => import("./ErrorInfo"));
 const ErrorHintText = dynamic(() => import("./ErrorPageHint"));
@@ -24,18 +24,27 @@ export default function ErrorPage({
       title={errorMessage}
       desc="如有任何問題，歡迎詢問或回報給我們 ;w;"
       icon={faExclamationTriangle}
-      headerColor="red-600"
+      headerColor="bg-negative"
     >
       <div className="error-info">
         <ErrorInfo occurredDate={new Date()} occurredDetails={errorDetails} />
       </div>
-      <div className="error-hint mt-1.5 pt-1.5 border-t-2">
+      <div className="error-hint mt-5">
         <ErrorHintText />
       </div>
-      <div className="operation-buttons mt-3">
-        <BaseButtonGroup>
-          <RefreshButton className="bg-red-600">再試一次</RefreshButton>
-        </BaseButtonGroup>
+      <div className="operation-buttons mt-5 flex space-x-2 justify-between">
+        <RefreshButton
+          className="flex-1"
+          backgroundColor="bg-negative"
+          solidBorderColor="bg-negative"
+        />
+        <LargeButton
+          className="flex-1"
+          borderColor="border-negative"
+          textColor="text-negative"
+        >
+          錯誤回報
+        </LargeButton>
       </div>
     </HeaderPageCard>
   );
