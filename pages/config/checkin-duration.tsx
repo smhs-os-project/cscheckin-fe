12bfdbd36edacad18778765ce92e496e1b621c1a
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 21:13:59 +0800

    feat(pages/config/checkin-duration): RWD & Design

--- pages/config/checkin-duration.tsx
+++ pages/config/checkin-duration.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from "react";
 import React from "react";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 import HeaderPageCard from "../../components/Page/HeaderPageCard";
@@ -5,21 +6,33 @@ import DurationInput from "../../components/Elements/Input/DurationInput";
 import LargeButtonGroup from "../../components/Elements/Button/Group/LargeButtonGroup";
 import LargeButton from "../../components/Elements/Button/LargeButton";
 
+interface InputBoxProps {
+  children: ReactNode;
+}
+
+function InputBox({ children }: InputBoxProps) {
+  return (
+    <div className="flex flex-col space-y-2 md:mx-0 md:space-y-0 md:flex-row md:items-center md:justify-between">
+      {children}
+    </div>
+  );
+}
+
 function LateInputBox() {
   return (
-    <div className="flex items-center justify-between">
+    <InputBox>
       <div>開課後多久自動劃記遲到</div>
-      <DurationInput suffix="後" />
-    </div>
+      <DurationInput />
+    </InputBox>
   );
 }
 
 function EndInputBox() {
   return (
-    <div className="flex items-center justify-between">
+    <InputBox>
       <div>開課後多久自動關閉簽到</div>
-      <DurationInput suffix="後" />
-    </div>
+      <DurationInput />
+    </InputBox>
   );
 }
 
@@ -31,7 +44,7 @@ export default function CheckinDurationConfig() {
       desc="設定開課後多久屬於遲到，多久結束簽到。您仍然可以手動結束簽到。"
       icon={faCog}
     >
-      <div className="flex flex-col space-y-4 mb-8">
+      <div className="flex flex-col space-y-10 mb-8 mx-2 md:mx-0">
         <LateInputBox />
         <EndInputBox />
       </div>
