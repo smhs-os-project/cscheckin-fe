2ee49cc1172cc3fb29c92304d3020c61319470d4
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 08:02:43 +0800

    feat(components/Navbar/PrevIcon): implement "Back to Previous" feature

--- components/Navbar/PrevIcon.tsx
+++ components/Navbar/PrevIcon.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
 
 export interface PrevIconProps {
   show: boolean;
 }
 
 export default function PrevIcon({ show }: PrevIconProps) {
+  const router = useRouter();
+
   if (show)
     return (
       <button
         type="button"
         className="rounded-lg hover:bg-secondary-hover p-4 transition-all duration-300"
+        onClick={async () => router.back()}
       >
         <FontAwesomeIcon icon={faAngleLeft} />
       </button>
     );
+
   return null;
 }
