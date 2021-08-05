20005dd6333b199ca316a06ab382ac0144502b02
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 08:03:08 +0800

    feat(components/Navbar/NavbarIcon): make the logo a button to /home

--- components/Navbar/NavbarIcon.tsx
+++ components/Navbar/NavbarIcon.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import icon from "../../public/icon.svg";
 
 export default function NavbarIcon() {
   return (
-    <div className="flex font-header items-center space-x-3 text-h2">
-      <Image src={icon as StaticImageData} width="36em" height="36em" />
-      <div>CSCheckin</div>
-    </div>
+    <Link href="/welcome">
+      <button
+        type="button"
+        className="flex font-header items-center space-x-3 text-h2 p-3"
+      >
+        <Image src={icon as StaticImageData} width="36em" height="36em" />
+        <div>CSCheckin</div>
+      </button>
+    </Link>
   );
 }
