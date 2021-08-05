fd0cd66dd17700dc9a6783fc3a0b7e231847e9b6
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-05 20:40:23 +0800

    feat(pages/config/checkin-duration): done

---
+++ pages/config/checkin-duration.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
+import HeaderPageCard from "../../components/Page/HeaderPageCard";
+import DurationInput from "../../components/Elements/Input/DurationInput";
+import LargeButtonGroup from "../../components/Elements/Button/Group/LargeButtonGroup";
+import LargeButton from "../../components/Elements/Button/LargeButton";
+
+function LateInputBox() {
+  return (
+    <div className="flex items-center justify-center">
+      <div>開課</div>
+      <DurationInput />
+      <div>後自動劃記遲到</div>
+    </div>
+  );
+}
+
+function EndInputBox() {
+  return (
+    <div className="flex items-center justify-center">
+      <div>開課</div>
+      <DurationInput />
+      <div>後自動關閉簽到</div>
+    </div>
+  );
+}
+
+// WIP: RWD & UI optimize
+export default function CheckinDurationConfig() {
+  return (
+    <HeaderPageCard
+      title="設定遲到與結束簽到時間"
+      desc="設定開課後多久屬於遲到，多久結束簽到。您仍然可以手動結束簽到。"
+      icon={faCog}
+    >
+      <div className="flex flex-col space-y-4 mb-8">
+        <LateInputBox />
+        <EndInputBox />
+      </div>
+      <LargeButtonGroup>
+        <LargeButton solid full>
+          儲存
+        </LargeButton>
+      </LargeButtonGroup>
+    </HeaderPageCard>
+  );
+}
