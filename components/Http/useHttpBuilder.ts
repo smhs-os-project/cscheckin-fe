369c29c7e175f031a9b5bb255b6f808d652a6fc4
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-07 22:12:18 +0800

    refactor(components/Http/useHttpBuilder): dynamically get the type of <auth>

--- components/Http/useHttpBuilder.ts
+++ components/Http/useHttpBuilder.ts
@@ -1,17 +1,20 @@
 import { useEffect } from "react";
 import useSWR from "swr";
-import type CSCAuth from "cscheckin-js-sdk/dist/auth";
 import useError from "../../utilities/ErrorReporting/useError";
 import type { HttpResponse } from "./HttpResponse";
 
-export default function useHttpBuilder<Payload = unknown, Response = unknown>(
+export default function useHttpBuilder<
+  Payload = null,
+  Auth = null,
+  Response = unknown
+>(
   key: string,
   asyncFunction: (
     key: string,
     auth?: CSCAuth,
     payload?: Payload
   ) => Promise<Response>,
-  auth?: CSCAuth,
+  auth?: Auth,
   payload?: Payload
 ): HttpResponse<Response> {
   const [error, setError] = useError();
