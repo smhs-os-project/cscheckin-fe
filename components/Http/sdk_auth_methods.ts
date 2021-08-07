487f2139978901ce5c10776ecc2db492088e96c7
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-07 22:27:24 +0800

    refactor(components/Http/sdk_auth_methods): explicit namespace

--- components/Http/sdk_auth_methods.ts
+++ components/Http/sdk_auth_methods.ts
@@ -2,7 +2,7 @@ import { GetClientId } from "cscheckin-js-sdk";
 import useHttpBuilder from "./useHttpBuilder";
 
 export const useClientId = () =>
-  useHttpBuilder("client_id", async () => {
+  useHttpBuilder("auth/client_id", async () => {
     const clientId = await GetClientId();
     return clientId.client_id;
   });
