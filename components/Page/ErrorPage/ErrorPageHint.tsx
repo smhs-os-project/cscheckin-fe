5ee567b04b024e314044f0d292467b0b0eb605d5
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-09 01:40:10 +0800

    feat(components/Page/ErrorPage/Hint): add more hints

--- components/Page/ErrorPage/ErrorPageHint.tsx
+++ components/Page/ErrorPage/ErrorPageHint.tsx
@@ -11,8 +11,22 @@ export default function ErrorPageHint() {
             （瀏覽器通常將這個功能稱之為「『嚴格』反追蹤模式」）
           </span>
         </li>
-        <li>瀏覽器版本是否已經更新至最新版本</li>
-        <li>作業系統是否版本太舊（如 Windows XP、Mac OS X 10.6 等）</li>
+        <li>
+          假如使用 LINE 或 Facebook 開啟，請確定
+          <b>是否已經使用外部瀏覽器開啟</b>
+        </li>
+        <li>
+          瀏覽器是否已經更新至最新版本
+          <span className="text-auxiliary">
+            （建議使用 Chrome、Edge、Firefox 或 Safari）
+          </span>
+        </li>
+        <li>
+          作業系統是否版本太舊
+          <span className="text-auxiliary">
+            （如 Windows XP、Mac OS X 10.6 等）
+          </span>
+        </li>
         <li>是否操作失誤</li>
       </ul>
       <p>您可以點擊下方的「再試一次」返回原頁面重新操作。</p>
