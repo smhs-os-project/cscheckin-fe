9708572ba0a52a5c51011ffc513224a564708585
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-09 01:41:04 +0800

    feat(utilities/ErrorReporting/useError): automatically convert object to Error
    fix: don't set Error if rawError is not defined

--- utilities/ErrorReporting/useError.ts
+++ utilities/ErrorReporting/useError.ts
@@ -7,13 +7,17 @@ export function unknownToError(error: unknown): Error {
     return error;
   }
 
+  if (typeof error === "object") {
+    return new Error(JSON.stringify(error));
+  }
+
   return new Error(`${error}`);
 }
 
 export default function useError(): [Error | null, PushErrorHandler] {
   const [error, setError] = useState<Error>();
   const pushErrorHandler: PushErrorHandler = (rawError) => {
-    setError(unknownToError(rawError));
+    if (rawError) setError(unknownToError(rawError));
   };
 
   return [error ?? null, pushErrorHandler];
