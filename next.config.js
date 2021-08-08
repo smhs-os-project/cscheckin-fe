d9494f524f7dd8b0100d1865d9f0421bd4bc978d
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-09 01:41:18 +0800

    feat(config/next): redirect /sso to /sso/teacher

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 exports.default = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/sso",
+        destination: "/sso/teacher",
+        permanent: false,
+      },
+    ];
+  },
 };
