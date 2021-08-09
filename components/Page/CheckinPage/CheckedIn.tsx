0fe36fc402273ecd4bfc0dff0f1b79aaeba6e65c
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-09 16:46:52 +0800

    feat(pages/checkin/[uuid]): done

---
+++ components/Page/CheckinPage/CheckedIn.tsx
@@ -0,0 +1,49 @@
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import React from "react";
+import type { CheckinResponse } from "cscheckin-js-sdk/dist/types";
+import dynamic from "next/dynamic";
+import { useUserInfo } from "../../Http/sdk_auth_methods";
+import type AuthenticatedPageProps from "../../Database/AuthStore/AuthenticatedPageProps";
+
+const LargeButton = dynamic(() => import("../../Elements/Button/LargeButton"));
+const HeaderPageCard = dynamic(() => import("../HeaderPageCard"));
+
+interface CheckedInProps extends AuthenticatedPageProps {
+  checkinData: CheckinResponse;
+}
+
+export default function CheckedIn({ auth, checkinData }: CheckedInProps) {
+  const { data: userInfo } = useUserInfo(auth);
+  const entry = (title: string, value: unknown) =>
+    value && (
+      <tr className="mb-2">
+        <td className="text-auxiliary">{title}</td>
+        <td>{`${value}`}</td>
+      </tr>
+    );
+
+  return (
+    <HeaderPageCard
+      title="Perfect!"
+      desc="簽到完成。您可保存此簽到憑據。"
+      headerColor="green-900"
+      icon={faCheck}
+    >
+      <table className="mb-2">
+        {entry("簽到 ID", checkinData.id)}
+        {entry("課程 ID", checkinData.course_id)}
+        {entry("學生 ID", checkinData.student_id)}
+        {entry("簽到", checkinData.state)}
+        {entry("姓名", userInfo?.name)}
+        {entry("信箱", userInfo?.email)}
+        {entry("班級", userInfo && (userInfo.student?.class ?? "⚠️ 未設定"))}
+        {entry("座號", userInfo && (userInfo.student?.number ?? "⚠️ 未設定"))}
+        {entry("時間", new Date(checkinData.updated_at).toLocaleString())}
+      </table>
+      <Link href="/config/info">
+        <LargeButton>重設班級座號</LargeButton>
+      </Link>
+    </HeaderPageCard>
+  );
+}
---
+++ pages/checkin/[uuid].tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import NProgress from "nprogress";
+import dynamic from "next/dynamic";
+import useAuth from "../../components/Database/AuthStore/useAuth";
+import { Scope } from "../../components/OAuth/Google/scope";
+import type AuthenticatedPageProps from "../../components/Database/AuthStore/AuthenticatedPageProps";
+import { useCheckin } from "../../components/Http/sdk_checkin_methods";
+import useQueryParam from "../../components/Hooks/useQueryParam";
+
+const AuthErrorPage = dynamic(
+  () => import("../../components/Database/AuthStore/AuthErrorPage")
+);
+const ErrorPage = dynamic(() => import("../../components/Page/ErrorPage"));
+const LoadingPage = dynamic(() => import("../../components/Page/LoadingPage"));
+const CheckedIn = dynamic(
+  () => import("../../components/Page/CheckinPage/CheckedIn")
+);
+
+interface PreparedCSCStudentCheckinProps extends AuthenticatedPageProps {
+  uuid: string;
+}
+
+export function AuthenticatedCSCStudentCheckin({
+  auth,
+  uuid,
+}: PreparedCSCStudentCheckinProps) {
+  const { data, error } = useCheckin(uuid, auth);
+
+  if (error)
+    return (
+      <ErrorPage
+        errorMessage="您未加入這間教室，或已經結束簽到。"
+        errorDetails={error.message}
+      />
+    );
+
+  if (data) {
+    return <CheckedIn auth={auth} checkinData={data} />;
+  }
+
+  return <LoadingPage reason="正在簽到⋯⋯" />;
+}
+
+export default function CSCStudentCheckin() {
+  const { auth, error } = useAuth(Scope.Student);
+  const {
+    value: uuid,
+    loading,
+    notSpecified: uuidNotSpecified,
+  } = useQueryParam("uuid");
+
+  useEffect(() => {
+    if (!loading && auth) NProgress.done();
+    else NProgress.start();
+  }, [auth, loading]);
+
+  if (error) return <AuthErrorPage authError={error} />;
+
+  if (uuidNotSpecified)
+    return <ErrorPage errorMessage="未指定課程的唯一編號。" />;
+
+  if (auth && uuid) {
+    return <AuthenticatedCSCStudentCheckin auth={auth} uuid={uuid} />;
+  }
+
+  return <LoadingPage reason="正在準備簽到介面⋯⋯" />;
+}
