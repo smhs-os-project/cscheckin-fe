ce7d00ffc6daca7485e240d71d6e765b468fdbf5
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-10 02:46:55 +0800

    feat(pages/checkin/manage): completed index & previous links

---
+++ pages/checkin/manage/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { faUserCheck } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+
+const ListChoicePageCard = dynamic(
+  () => import("../../../components/Page/ListChoicePageCard")
+);
+
+export default function CheckinManageIndexPage() {
+  const router = useRouter();
+
+  return (
+    <ListChoicePageCard
+      title="簽到連結管理選單"
+      desc="建立簽到連結，還是查看以往的簽到連結呢？"
+      icon={faUserCheck}
+      choice={[
+        {
+          id: "create-new",
+          name: "建立新的簽到連結",
+          redirect: async () => router.push("/checkin/manage/create"),
+        },
+        {
+          id: "see-previous",
+          name: "查看以往簽到連結",
+          redirect: async () => router.push("/checkin/manage/previous"),
+        },
+      ]}
+    />
+  );
+}
---
+++ pages/checkin/manage/previous/index.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import NProgress from "nprogress";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import useAuth from "../../../../components/Database/AuthStore/useAuth";
+import AuthErrorPage from "../../../../components/Database/AuthStore/AuthErrorPage";
+import LoadingPage from "../../../../components/Page/LoadingPage";
+import type AuthenticatedPageProps from "../../../../components/Database/AuthStore/AuthenticatedPageProps";
+import { useCoursesList } from "../../../../components/Http/sdk_course_methods";
+import ErrorPage from "../../../../components/Page/ErrorPage";
+import ListChoicePageCard from "../../../../components/Page/ListChoicePageCard";
+
+export function AuthenticatedCheckinManagePreviousPage({
+  auth,
+}: AuthenticatedPageProps) {
+  const { data, error } = useCoursesList(auth);
+
+  if (error)
+    return (
+      <ErrorPage
+        errorMessage="無法取得簽到課程資料。"
+        errorDetails={error.message}
+      />
+    );
+
+  if (data) {
+    return (
+      <ListChoicePageCard
+        title="查看以往簽到記錄"
+        desc="依開課時間新到舊排序。"
+        icon={faSearch}
+        choice={data.reverse().map((entry) => ({
+          id: `previous-courses-entry-${entry.google_classroom_id}-${entry.id}`,
+          name: `${entry.name} - ${entry.start_timestamp.toLocaleString(
+            "zh-TW"
+          )}`,
+          redirect: () => `/checkin/manage/dashboard/${entry.id}`,
+        }))}
+      />
+    );
+  }
+
+  return <LoadingPage reason="正在載入簽到課程名單⋯⋯" />;
+}
+
+export default function CheckinManagePreviousPage() {
+  const { auth, error } = useAuth();
+
+  useEffect(() => {
+    if (auth || error) NProgress.done();
+    else NProgress.start();
+  }, [auth, error]);
+
+  if (error) return <AuthErrorPage authError={error} />;
+  if (auth) return <AuthenticatedCheckinManagePreviousPage auth={auth} />;
+
+  return <LoadingPage reason="正在取得登入資訊⋯⋯" />;
+}
