805c0031cdf359cd8feb03035ef5afab3eec5a93
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-09 11:41:37 +0800

    feat(components/Database/AuthStore/useAuth): redirect to the specified scope of /sso if redirect is specified

--- components/Database/AuthStore/useAuth.ts
+++ components/Database/AuthStore/useAuth.ts
@@ -1,6 +1,8 @@
 import type CSCAuth from "cscheckin-js-sdk/dist/auth";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import useError from "../../../utilities/ErrorReporting/useError";
+import { Scope } from "../../OAuth/Google/scope";
 import AuthStore from "./index";
 
 const authStore = AuthStore.getCommonInstance();
@@ -11,7 +13,8 @@ export interface AuthResponse {
   pending: boolean;
 }
 
-export default function useAuth(): AuthResponse {
+export default function useAuth(redirect?: Scope): AuthResponse {
+  const router = useRouter();
   const [auth, setAuth] = useState<CSCAuth>();
   const [error, setError] = useError();
 
@@ -19,8 +22,21 @@ export default function useAuth(): AuthResponse {
     authStore
       .getAuth()
       .then((receivedAuth) => setAuth(receivedAuth))
-      .catch((e) => setError(e));
-  }, [setError]);
+      .catch((e) => {
+        setError(e);
+
+        switch (redirect) {
+          case Scope.Student:
+            return router.push("/sso/student");
+          case Scope.Teacher:
+            return router.push("/sso/teacher");
+          default:
+            break;
+        }
+
+        return undefined;
+      });
+  }, [setError, redirect, router]);
 
   return {
     auth: auth ?? null,
