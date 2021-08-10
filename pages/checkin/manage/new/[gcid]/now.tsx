41aeb47c5c6af7a8fde8d4bc9114516926ac9232
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-10 09:39:40 +0800

    feat(pages/checkin/manage/new/[gcid]/now): rewrote with normal POST way

--- pages/checkin/manage/new/[gcid]/now.tsx
+++ pages/checkin/manage/new/[gcid]/now.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import NProgress from "nprogress";
-import { useCreateCourse } from "../../../../../components/Http/sdk_course_methods";
+import type { CourseResponse } from "cscheckin-js-sdk/dist/types";
+import { CreateCourse } from "cscheckin-js-sdk";
 import useQueryParam from "../../../../../components/Hooks/useQueryParam";
 import type AuthenticatedPageProps from "../../../../../components/Database/AuthStore/AuthenticatedPageProps";
 import LoadingPage from "../../../../../components/Page/LoadingPage";
@@ -9,6 +9,7 @@ import ErrorPage from "../../../../../components/Page/ErrorPage";
 import useAuth from "../../../../../components/Database/AuthStore/useAuth";
 import { Scope } from "../../../../../components/OAuth/Google/scope";
 import AuthErrorPage from "../../../../../components/Database/AuthStore/AuthErrorPage";
+import useError from "../../../../../utilities/ErrorReporting/useError";
 
 interface AuthenticatedCheckinManageNewCourseNowProps
   extends AuthenticatedPageProps {
@@ -20,21 +21,28 @@ function AuthenticatedCheckinManageNewCourseNow({
   auth,
 }: AuthenticatedCheckinManageNewCourseNowProps) {
   const router = useRouter();
-  const { data, error } = useCreateCourse(
-    {
-      classroomId,
-      start_timestamp: new Date(),
-      late_time: "00:15:00",
-      expire_time: "01:00:00",
-    },
-    auth
-  );
+  const [error, setError] = useError();
+  const [data, setData] = useState<CourseResponse>();
+
+  useEffect(() => {
+    if (!data && !error)
+      void CreateCourse(
+        classroomId,
+        {
+          start_timestamp: new Date(),
+          late_time: "00:15:00",
+          expire_time: "01:00:00",
+        },
+        auth
+      )
+        .then((response) => setData(response))
+        .catch((recvError: unknown) => setError(recvError));
+  }, [auth, classroomId, setError, data, error]);
 
   useEffect(() => {
     if (data) {
-      NProgress.done();
       void router.push(`/checkin/manage/dashboard/${data.id}`);
-    } else if (!data && !error) NProgress.start();
+    }
   }, [data, error, router]);
 
   if (error)
