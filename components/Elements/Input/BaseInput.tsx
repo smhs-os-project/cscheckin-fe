38e43f18ad8683301749768ad9a856711c0aaeb9
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-10 15:11:03 +0800

    refactor(components/Elements/Input/BaseInput): tweak the design

--- components/Elements/Input/BaseInput.tsx
+++ components/Elements/Input/BaseInput.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-export interface BaseInputsProps {
+export interface BaseInputsProps
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "onChange"
+  > {
   label: string;
   value?: string;
   onChange?: (value: string) => void;
@@ -12,11 +19,15 @@ export default function BaseInput({
   value,
   onChange,
   backgroundColor = "bg-on-surface",
+  className,
+  ...props
 }: BaseInputsProps) {
   return (
     <input
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
       type="input"
-      className={`px-6 py-4 ${backgroundColor} rounded-t-lg under-shadow tracking-button`}
+      className={`px-4 py-2 ${backgroundColor} rounded-t-lg under-shadow tracking-button ${className}`}
       placeholder={label}
       onChange={(event) => onChange && onChange(event.target.value)}
       value={value}
