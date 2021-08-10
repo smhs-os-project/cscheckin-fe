6379b1e33391ad6e93164b40873d9be6432c1093
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-10 16:15:41 +0800

    feat(components/Page/Dashboard/Card): allow to configure the dashboard card

--- components/Page/Dashboard/DashboardCard/DashboardCard.stories.tsx
+++ components/Page/Dashboard/DashboardCard/DashboardCard.stories.tsx
@@ -1,16 +1,36 @@
 import React from "react";
 import type { Meta, Story } from "@storybook/react";
-import DashboardCard from "./DashboardCard";
+import CSCAuth from "cscheckin-js-sdk/dist/auth";
 import type { DashboardCardProps } from "./DashboardCard";
+import DashboardCard from "./DashboardCard";
+import { StatusCardStatus } from "./StatusCard";
 
 export default {
   title: "Page/Dashboard/DashboardCard",
   component: DashboardCard,
 } as Meta;
 
-const Template: Story<DashboardCardProps> = (props: DashboardCardProps) => (
-  <DashboardCard />
+const Template: Story<DashboardCardProps> = ({
+  auth,
+  cid,
+  link,
+  status,
+  onFullScreen,
+}: DashboardCardProps) => (
+  <DashboardCard
+    auth={auth}
+    cid={cid}
+    status={status}
+    link={link}
+    onFullScreen={onFullScreen}
+  />
 );
 
 export const DashboardCardExample = Template.bind({});
-DashboardCardExample.args = {};
+DashboardCardExample.args = {
+  auth: new CSCAuth("", ""),
+  cid: 0,
+  link: "https://cscin.tk/1cc1f2",
+  status: StatusCardStatus.OPEN,
+  onFullScreen: () => null,
+};
--- components/Page/Dashboard/DashboardCard/DashboardCard.tsx
+++ components/Page/Dashboard/DashboardCard/DashboardCard.tsx
@@ -1,25 +1,36 @@
 import React from "react";
-import CSCAuth from "cscheckin-js-sdk/dist/auth";
 import LargeButtonGroup from "../../../Elements/Button/Group/LargeButtonGroup";
-import StatusCard, { StatusCardStatus } from "./StatusCard";
+import type { StatusCardProps } from "./StatusCard";
+import StatusCard from "./StatusCard";
+import type { CourseLinkBlockProps } from "./CourseLinkBlock";
 import CourseLinkBlock from "./CourseLinkBlock";
+import type { ClassroomShareButtonProps } from "./ClassroomShareButton";
 import ClassroomShareButton from "./ClassroomShareButton";
 import CloseCourseButton from "./CloseCourseButton";
 import CopyLinkButton from "./CopyLinkButton";
 
-export interface DashboardCardProps {}
+export interface DashboardCardProps
+  extends CourseLinkBlockProps,
+    StatusCardProps,
+    ClassroomShareButtonProps {}
 
-export default function DashboardCard(props: DashboardCardProps) {
+export default function DashboardCard({
+  auth,
+  cid,
+  link,
+  status,
+  onFullScreen,
+}: DashboardCardProps) {
   return (
     <section className="dashboard-card rounded-xl bg-secondary overflow-hidden">
-      <StatusCard status={StatusCardStatus.OPEN} />
+      <StatusCard status={status} />
       <div className="flex flex-col space-y-4 m-8">
-        <CourseLinkBlock link="hi" />
+        <CourseLinkBlock link={link} onFullScreen={onFullScreen} />
         <LargeButtonGroup>
-          <ClassroomShareButton cid={123} auth={new CSCAuth("", "")} />
-          <CopyLinkButton link="Hi" />
+          <ClassroomShareButton cid={cid} auth={auth} />
+          <CopyLinkButton link={link} />
         </LargeButtonGroup>
-        <CloseCourseButton cid={123} auth={new CSCAuth("", "")} />
+        <CloseCourseButton cid={cid} auth={auth} />
       </div>
     </section>
   );
