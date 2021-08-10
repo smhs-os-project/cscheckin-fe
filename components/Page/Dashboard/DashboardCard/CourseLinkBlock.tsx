864687a5b1b47c88774cef8742dfff7fa2f3da78
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-10 16:15:06 +0800

    feat(components/Page/Dashboard/Card/CourseLinkBlock): add an full-screen icon

--- components/Page/Dashboard/DashboardCard/CourseLinkBlock.tsx
+++ components/Page/Dashboard/DashboardCard/CourseLinkBlock.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import QRCode from "react-qr-code";
+import { faExpand } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BaseInput from "../../../Elements/Input/BaseInput";
 
 export interface CourseLinkBlockProps {
@@ -7,26 +9,36 @@ export interface CourseLinkBlockProps {
   onFullScreen?: () => void;
 }
 
-export default function CourseLinkBlock({ link }: CourseLinkBlockProps) {
+export default function CourseLinkBlock({
+  link,
+  onFullScreen,
+}: CourseLinkBlockProps) {
   return (
-    <section className="bg-on-surface flex flex-col sm:flex-row justify-between justify-center items-center p-8 sm:space-x-8 rounded-xl">
-      <div className="mx-auto">
-        <div className="block">
+    <>
+      <section className="relative bg-on-surface flex flex-col sm:flex-row justify-evenly items-center p-8 sm:space-x-16 rounded-xl">
+        <button
+          type="button"
+          className="absolute top-4 right-4"
+          onClick={() => onFullScreen && onFullScreen()}
+        >
+          <FontAwesomeIcon icon={faExpand} size="lg" />
+        </button>
+        <div className="mx-auto">
           <QRCode size={144} value={link} />
         </div>
-      </div>
-      <div className="overflow-hidden mt-4 sm:mt-0 text-center sm:text-left sm:ml-8">
-        <p className="text-auxiliary">透過以下連結簽到</p>
-        <BaseInput
-          label="連結"
-          className="font-header sm:text-h1 text-center sm:text-left bg-transparent"
-          value={link}
-          readOnly
-          onFocus={(e) => {
-            e.target.select();
-          }}
-        />
-      </div>
-    </section>
+        <div className="overflow-hidden mt-4 sm:mt-0 text-center sm:text-left sm:ml-8 flex-grow">
+          <p className="text-auxiliary">透過以下連結簽到</p>
+          <BaseInput
+            label="連結"
+            className="font-header sm:text-h1 text-center sm:text-left bg-transparent"
+            value={link}
+            readOnly
+            onFocus={(e) => {
+              e.target.select();
+            }}
+          />
+        </div>
+      </section>
+    </>
   );
 }
