7425ddbfb61996e497ece4c2f98697aef7d97c53
Yi-Jyun Pan <pan93412@gmail.com>
2021-08-11 11:29:59 +0800

    feat(components/Page/ErrorPage/index): use Sentry.showReportDialog()

--- components/Page/ErrorPage/index.tsx
+++ components/Page/ErrorPage/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import dynamic from "next/dynamic";
-import { useRouter } from "next/router";
+import * as Sentry from "@sentry/browser";
 import HeaderPageCard from "../HeaderPageCard";
 import LargeButton from "../../Elements/Button/LargeButton";
 import LargeButtonGroup from "../../Elements/Button/Group/LargeButtonGroup";
@@ -25,7 +25,6 @@ const NegativeRefreshButton = () => (
 );
 
 const NegativeErrorReportingButton = () => {
-  const router = useRouter();
   const { auth } = useAuth();
 
   return (
@@ -37,14 +36,20 @@ const NegativeErrorReportingButton = () => {
       onClick={async () => {
         const userInfo = await auth?.userInfo();
 
-        window.open(
-          `https://cscin.tk/?action=feedback&name=${encodeURI(
-            userInfo?.name ?? ""
-          )}&email=${encodeURI(userInfo?.email ?? "")}&path=${encodeURI(
-            router.asPath
-          )}`,
-          "_blank"
-        );
+        Sentry.showReportDialog({
+          user: {
+            email: userInfo?.email,
+            name: userInfo?.name,
+          },
+          title: "請告知我們問題相關資訊。",
+          subtitle: "比如說你操作了什麼，或者是設定了什麼。",
+          subtitle2: "我們會立刻處理。",
+          labelName: "您的暱稱",
+          labelEmail: "您的 email",
+          labelComments: "問題描述（附圖請用 imgur）",
+          labelClose: "關閉",
+          labelSubmit: "回報",
+        });
       }}
     >
       問題回報
